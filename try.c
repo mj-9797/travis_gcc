9b59f3b6d9b6171cb2d952b0b5b692e21811f790
mj-9797 <70877266+mj-9797@users.noreply.github.com>
2020-11-26 18:36:41 +0530

    Update try.c

--- try.c
+++ try.c
@@ -2,8 +2,10 @@
 int main()
 {
 #ifdef __GNUG__
+                printf("qwertyuio");
                 printf("I am wrong");
 #else
+                printf ("123456789");
                 printf("I am right");
 #endif
         return 0;
